b4bc92c118820b25bb5ac2731a669e5be9f3ef56
MERORO911 <adamjustin616219@gmail.com>
2025-06-01 21:54:58 +0800

    購物車細節展開收合

--- Papaya/app/src/main/java/ppy/app/papaya/CartItemAdapter.java
+++ Papaya/app/src/main/java/ppy/app/papaya/CartItemAdapter.java
@@ -15,14 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.List;
 
