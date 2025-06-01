21c0c0bfb27a129e5b35fd3490546c0166832428
Sarah0423 <tsh042393@gmail.com>
2025-06-01 12:41:31 +0800

    修正主畫面

--- Papaya/app/src/main/java/ppy/app/papaya/CheckCart.java
+++ Papaya/app/src/main/java/ppy/app/papaya/CheckCart.java
@@ -1,14 +1,32 @@
 package ppy.app.papaya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckCart extends AppCompatActivity {
+
+    private ImageButton btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,17 @@ public class CheckCart extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnReturn = findViewById(R.id.btn_return);
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckCart.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
