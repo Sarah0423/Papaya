e987f7935f136830538c5f6c6badaafe70f8ec2a
MERORO911 <adamjustin616219@gmail.com>
2025-06-02 15:07:30 +0800

    0

---
+++ Papaya/app/src/main/java/ppy/app/papaya/AlarmList.java
@@ -0,0 +1,36 @@
+package ppy.app.papaya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AlarmList extends AppCompatActivity {
+
+    private ImageButton btnReturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alarm_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnReturn = findViewById(R.id.btn_return);
+        btnReturn.setOnClickListener(v -> {
+            Intent intent = new Intent(AlarmList.this, MainActivity.class);
+            intent.putExtra("SHOW_FUNCTION_MENU", true);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
