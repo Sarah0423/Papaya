ce137e3e9342dab69c5d326e46141dd08d9fdd12
MERORO911 <adamjustin616219@gmail.com>
2025-06-08 19:09:40 +0800

    DeliveryStatus

---
+++ Papaya/app/src/main/java/ppy/app/papaya/DeliveryStatus.java
@@ -0,0 +1,45 @@
+package ppy.app.papaya;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeliveryStatusActivity extends AppCompatActivity {
+
+    private TextView etaText;
+    private ProgressBar progressBar;
+
+    private static final int TOTAL_TIME = 5 * 60 * 1000; // 5 分鐘
+    private static final int INTERVAL = 1000; // 每秒更新一次
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_status);
+
+        etaText = findViewById(R.id.etaText);
+        progressBar = findViewById(R.id.progressBar);
+
+        startCountdown();
+    }
+
+    private void startCountdown() {
+        new CountDownTimer(TOTAL_TIME, INTERVAL) {
+            public void onTick(long millisUntilFinished) {
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                etaText.setText("預計送達時間：" + minutes + " 分 " + seconds + " 秒");
+
+                int progress = (int) (millisUntilFinished * 100 / TOTAL_TIME);
+                progressBar.setProgress(progress);
+            }
+
+            public void onFinish() {
+                etaText.setText("外送已送達！");
+                progressBar.setProgress(0);
+            }
+        }.start();
+    }
+}
