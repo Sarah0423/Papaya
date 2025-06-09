be8865a90f44c82f5261313b3172b7e18cb651ca
MERORO911 <adamjustin616219@gmail.com>
2025-06-09 18:14:15 +0800

    0

--- Papaya/app/src/main/java/ppy/app/papaya/Login.java
+++ Papaya/app/src/main/java/ppy/app/papaya/Login.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,7 +34,7 @@ public class Login extends AppCompatActivity {
     private Button btnLogin;
     private ImageButton btnReturn;
     private FirebaseAuth mAuth;
-    private TextView tvForgetPassword;
+    private TextView tvForgetPassword, tvForget_Password;
     private TextView tvLoginSignin;
 
     @Override
@@ -54,11 +55,23 @@ public class Login extends AppCompatActivity {
         etPassword = findViewById(R.id.et_login_password);
         btnLogin = findViewById(R.id.btn_login);
         btnReturn = findViewById(R.id.btn_return);
+        tvForget_Password = findViewById(R.id.tv_forget_password);
 
         tvForgetPassword = findViewById(R.id.tv_forget_password);
         tvLoginSignin = findViewById(R.id.tv_login_signin);
         tvForgetPassword.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         tvLoginSignin.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        tvForget_Password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String serviceNumber = "000000000";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + serviceNumber));
+                startActivity(intent);
+                Toast.makeText(Login.this, "請洽客服人員，我們將為您服務！", Toast.LENGTH_LONG).show();
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
